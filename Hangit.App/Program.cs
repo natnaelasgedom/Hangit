1b380bacd5fee8bb8db393353c6795508cc3ab0f
natnaelasgedom <natnael.asgedom@gmail.com>
2020-06-10 12:47:28 +0200

    Revert "Deleted a method"
    This reverts commit df9de4c6367ee3682515c886608973534bcd4028.

--- Hangit.App/Program.cs
+++ Hangit.App/Program.cs
@@ -35,7 +35,11 @@ namespace Hangit.App
             return Regex.IsMatch(userGuess.ToUpper(), "^[A-ZÆØÅ]$");
         }
 
-      
+        public static bool RightCharacter(string userGuess)
+        {
+            return Regex.IsMatch(userGuess.ToUpper(), "^[GODFATHER]$");
+        }
+
         
     }
 }
