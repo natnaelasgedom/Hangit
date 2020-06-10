3b2bbdeea9c9d3cccf55f3e01176365c9299db64
natnaelasgedom <natnael.asgedom@gmail.com>
2020-06-10 14:11:43 +0200

    leaving the loop when out of guesses

--- Hangit.App/Program.cs
+++ Hangit.App/Program.cs
@@ -1,40 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace Hangit.App
 {
     class Program
     {
+        public static string secretWord = "GODFATHER";
         static void Main(string[] args)
         {
-            string secretWord = "GODFATHER";
+            //List<char> guessedCharacters = new List<char>();
+            int guessesLeft = 10;
             do
             {
-                Console.WriteLine("Your guess: ");
+                Console.WriteLine();
+                Console.WriteLine($"Guesses left: {guessesLeft}");
+                Console.Write("Your guess: ");
                 string ans = Console.ReadLine();
                 Console.WriteLine($"Your guess: {ans}");
-
-                if (!ValidGuess(ans))
+                guessesLeft--;
+                if (ValidGuess(ans))
                 {
-                    Console.WriteLine("Invalid guess");
-                    Console.ReadLine();
-                    Console.Clear();
-                }
-                else if (RightCharacter(ans))
-                {
-                    Console.WriteLine("Correct");
-                    Console.ReadLine();
-                    Console.Clear();
-
+                    if (secretWord.Contains(ans))
+                    {
+                        Console.WriteLine("Correct");
+                    }
+                    
+                    else
+                    {
+                        Console.WriteLine("Wrong");
+                        //guessedCharacters.Add(ans[0]);
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Wrong");
-                    Console.ReadLine();
-                    Console.Clear();
-
+                    Console.WriteLine("Invalid guess");
                 }
-            } while (true);
+
+            } while (true && guessesLeft>0);
+            Console.WriteLine("GAME OVER");
             
             
             
@@ -49,9 +53,8 @@ namespace Hangit.App
 
         public static bool RightCharacter(string userGuess)
         {
-            return Regex.IsMatch(userGuess.ToUpper(), "^[GODFATHER]$");
+            return Regex.IsMatch(userGuess.ToUpper(), "^[+ secretWord + ]$");
         }
-
         
     }
 }
